79a0a44d9dd5417d0eeaa2f45d66db8a72ea0800
HanselCarpio <38017780+HanselCarpio@users.noreply.github.com>
2019-09-03 16:12:23 -0600

    cambios errores eb DB

--- basesPags.sql
+++ basesPags.sql
@@ -14,10 +14,10 @@ INSERT INTO listaP
 VALUES 
 ("1", "acer nitro" , 1200 , "negro", "laptop", "acer-nitro.jpg"),
 ("2", "Asus x512" , 1000 , "rojo", "laptop", "asus-x512.jpg"),
-("3", "dell inspiron" , 1500 , "blanca", "laptop", "dell-inspiron.jpg"),
+("3", "dell inspiron" , 1500 , "blancax", "blanca", "dell-inspiron.jpg"),
 ("4", "hp omen" , 1700 , "b&w", "laptop", "hp-omen.jpg"),
 ("5", "msi" , 2000 , "rgb", "laptop", "msi.jpg"),
-("6", "alienware" , 2200 , "laptop", "negro", "alienware.jpg")
+("6", "alienware" , 2200 , "negro", "laptop", "alienware.jpg")
 
 
 CREATE PROCEDURE sp_insertarArticulo
@@ -28,7 +28,7 @@ VALUES
 (idProduct, nameProduct , priceProduct, descProduct, category, imagLaptop)
 
 CREATE PROCEDURE sp_cargarArticulos()
-SELECT listaP.idPtoduct, listaP.nameProduct as articulo, listaP.priceProduct, listaP.descProduct, listaP.category, listaP.imagLaptop
+SELECT listaP.idProduct, listaP.nameProduct as articulo, listaP.priceProduct, listaP.descProduct, listaP.category, listaP.imagLaptop
 FROM articulo
 
 //////////////////////////////////////////////////////////////////////////
@@ -46,6 +46,15 @@ category varchar(50) NOT null,
 imagCamera varchar (50) not null
 )
 
+INSERT INTO listaP
+(idProduct, nameProduct , priceProduct, descProduct, category, imagCamera) 
+VALUES 
+("1", "5D mark IV" , 1200 , "negro", "camera", "5D_MarklV.jpg"),
+("2", "cybershot" , 1000 , "negro", "camera", "cybershot.jpg"),
+("3", "instamini" , 1500 , "blue", "camera", "instamini.jpg"),
+("4", "Nikon D610" , 1700 , "b&w", "camera", "nikon_d610.jpg"),
+("5", "Polaroid" , 2000 , "rgb", "camera", "polaroid.jpg"),
+("6", "PowerShot silver" , 2200 , "silver", "camera", "PowerShot_Silver.jpg")
 
 CREATE PROCEDURE sp_insertarArticulo
 (idPRoduct varchar(10), nameProduct varchar(50), priceProduct int, descProduct varchar(20), category varchar(50), imagLaptop varchar(50)) 
@@ -55,11 +64,10 @@ VALUES
 (idProduct, nameProduct , priceProduct, descProduct, category, imagLaptop)
 
 CREATE PROCEDURE sp_cargarArticulos()
-SELECT listaP.idPtoduct, listaP.nameProduct as articulo, listaP.priceProduct, listaP.descProduct, listaP.category, listaP.imagLaptop
+SELECT listaP.idProduct, listaP.nameProduct as articulo, listaP.priceProduct, listaP.descProduct, listaP.category, listaP.imagLaptop
 FROM articulo
 
-
-//////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
 
 create database hc_smartphones
 use hc_smartphones
@@ -72,7 +80,15 @@ category varchar(50) NOT null,
 imagSmartPhone varchar (50) not null
 )
 
-
+INSERT INTO listaP
+(idProduct, nameProduct , priceProduct, descProduct, category, imagLaptop) 
+VALUES 
+("1", "iphone XR" , 1200 , "negro", "smartphone", "iphone_XR.jpg"),
+("2", "LG Q60" , 1000 , "negro", "smartphone", "lg_q60.jpg"),
+("3", "mi 9" , 1500 , "negro", "smartphone", "mi9.jpg"),
+("4", "p smart" , 2000 , "rgb", "smartphone", "p_smart.jpg"),
+("5", "S 10" , 1700 , "b&w", "smartphone", "s10.jpg"),
+("6", "Xperia 10" , 2200 , "negro", "smartphone", "Xperia_10.jpg")
 
 CREATE PROCEDURE sp_insertarArticulo
 (idPRoduct varchar(10), nameProduct varchar(50), priceProduct int, descProduct varchar(20), category varchar(50), imagLaptop varchar(50)) 
@@ -82,11 +98,6 @@ VALUES
 (idProduct, nameProduct , priceProduct, descProduct, category, imagLaptop)
 
 CREATE PROCEDURE sp_cargarArticulos()
-SELECT listaP.idPtoduct, listaP.nameProduct as articulo, listaP.priceProduct, listaP.descProduct, listaP.category, listaP.imagLaptop
+SELECT listaP.idProduct, listaP.nameProduct as articulo, listaP.priceProduct, listaP.descProduct, listaP.category, listaP.imagLaptop
 FROM articulo
-
-
-
-
-
 
