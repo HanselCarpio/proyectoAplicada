997550834130d6a199f408133674835d81c8c3e6
HanselCarpio <38017780+HanselCarpio@users.noreply.github.com>
2019-09-07 15:37:21 -0600

    Update basePostgre.sql

--- basePostgre.sql
+++ basePostgre.sql
@@ -4,17 +4,17 @@ CREATE SCHEMA main
 CREATE TABLE main.clients
 (
     "idClient" integer NOT NULL unique,
-    "nameClient" character varying(50)[] NOT NULL,
-    "numID" character varying(15)[] NOT NULL,
-    "userClient" character varying(20)[] NOT NULL,
-    "passwordClient" character varying(30)[] NOT NULL,
+    "nameClient" varchar(50) NOT NULL,
+    "numID" varchar(15) NOT NULL,
+    "userClient" varchar(20) NOT NULL,
+    "passwordClient" varchar(30) NOT NULL,
     PRIMARY KEY ("idClient", "passwordClient")
 )
 
 CREATE TABLE main.creditCard
 (
 	"idClient" integer NOT NULL REFERENCES main.clients("idClient"),
-    "cardType" character varying(5)[] NOT NULL,
+    "cardType" varchar(5) NOT NULL,
     "cardNumber" integer NOT NULL,
     cvv integer NOT NULL,
 	PRIMARY KEY ("idClient")
@@ -23,17 +23,17 @@ CREATE TABLE main.creditCard
 CREATE TABLE main.address
 (
 	"idClient" integer NOT NULL REFERENCES main.clients("idClient"),
-    "details" character varying(100)[] NOT NULL,
+    "details" varchar(100) NOT NULL,
 	PRIMARY KEY ("idClient")
 )
 
 CREATE TABLE main.products
 (
 	"idProduct" integer NOT NULL unique,
-    "nameProduct" character varying(50)[] NOT NULL,
+    "nameProduct" varchar(50) NOT NULL,
     "priceProduct" integer NOT NULL,
-    "descProduct" character varying(100)[] NOT NULL,
-    "imageProduct" character varying(50)[] NOT NULL,
+    "descProduct" varchar(100) NOT NULL,
+    "imageProduct" varchar(50) NOT NULL,
 	"idCategory" integer NOT NULL REFERENCES main.category("idCategory"),
 	"status" char not null check(status= '[S]' or status='[s]' or status = '[n]' or status='[N]');
     PRIMARY KEY ("idProduct", "idCategory")
@@ -57,17 +57,17 @@ CREATE TABLE main.visits
 CREATE TABLE main.category
 (
 	"idCategory" integer NOT NULL,
-	"nameCategory" character varying(30)[] NOT NULL,
+	"nameCategory" varchar(30) NOT NULL,
 	PRIMARY KEY ("idCategory")
 )
 
 CREATE TABLE main.suppliers
 (
 	"idSupplier" integer NOT NULL,
-	"keySupplier" character varying(20)[] NOT NULL,
-	"nameSupplier" character varying(30)[] NOT NULL,
-	"phonoNumber" character varying(20)[] NOT NULL,
-	"email" character varying(40)[] NOT NULL, 
+	"keySupplier" varchar(20) NOT NULL,
+	"nameSupplier" varchar(30) NOT NULL,
+	"phonoNumber" varchar(20) NOT NULL,
+	"email" varchar(40) NOT NULL, 
 	PRIMARY KEY ("idSupplier")
 )
 
